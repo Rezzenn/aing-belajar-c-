3fbd3e594c8f83c5fd4f1d06d441807518f75b86
Rezzenn <rezaxxyyy@gmail.com>
2026-09-15 11:33:21 +0800

    perubahan

---
+++ tipedata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    double tinggi_saya = 170.5;
+    int usia = 16;
+
+    cout << "Usia Saya Adalah " << usia << endl;
+    cout << "Tinggi Saya Adalah " << tinggi_saya;
+    return 0;
+} 
+
+//Catatan,jika ingin membuat spasi agar angka nya tidak nyambung cukup kasih spasi di dalam string nya
