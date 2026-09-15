39b28a877be250864f75b7fdc0d092eea020fc17
Rezzenn <rezaxxyyy@gmail.com>
2026-09-15 11:45:01 +0800

    Dasar Tipe Data di

--- tipedata.cpp
+++ tipedata.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-
 using namespace std;
 int main() {
     double tinggi_saya = 170.5;
     int usia = 16;
+    const string nisn = "0209309209";
 
-    cout << "Usia Saya Adalah " << usia << endl;
-    cout << "Tinggi Saya Adalah " << tinggi_saya;
+    cout << "Usia Saya Adalah " << usia << " dan ";
+    cout << "Tinggi Saya Adalah " << tinggi_saya << endl;
+    cout << " NISN saya Adalah " << nisn;
     return 0;
 } 
 
